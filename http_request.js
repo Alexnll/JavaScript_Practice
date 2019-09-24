767460e5ab0b269ff231a9d1a812a821d06221dd
Alexnll <czxalexnl@gmail.com>
2019-09-24 23:24:06 +0800

    adding http request sending by node.js

---
+++ http_request.js
@@ -0,0 +1,11 @@
+var http=require("http");
+
+http.get("http://127.0.0.1:8000/pigmentSearch/",function(data){
+    var str="";
+    data.on("data",function(chunk){ // data: 当端接收到数据时触发
+        str+=chunk;//监听数据响应，拼接数据片段
+    })
+    data.on("end",function(){ // End: 数据接受完时触发
+        console.log(str.toString())
+    })
+})
