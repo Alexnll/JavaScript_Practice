94306f987cfa0065a4e46261593586b0508c0fa3
Alexnll <czxalexnl@gmail.com>
2019-10-10 23:36:00 +0800

    update http_request.js

--- http_request.js
+++ http_request.js
@@ -1,9 +1,13 @@
-var http=require("http");
+const http=require("http");
 
-http.get("http://127.0.0.1:8000/pigmentSearch/",function(data){
+const user_input = "yellow"
+const django_url = "http://127.0.0.1:8000/pigmentSearch/" + user_input + "/";
+const baidu_trial = "http://www.baidu.com/" 
+
+http.get(django_url,function(data){
     var str="";
     data.on("data",function(chunk){ // data: 当端接收到数据时触发
-        str+=chunk;//监听数据响应，拼接数据片段
+        str+=chunk; //监听数据响应，拼接数据片段
     })
     data.on("end",function(){ // End: 数据接受完时触发
         console.log(str.toString())
